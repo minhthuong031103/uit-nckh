eab2f55a62bfc6a9f757f56c40b76e402eb383b6
thuong.nguyen3 <nominh67@gmail.com>
2024-05-20 05:02:14 +0700

    base

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,51 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import {
+  ConnectWallet,
+  useSwitchChain,
+  getProviderFromRpcUrl,
+  useSDK,
+} from '@thirdweb-dev/react-native';
+import { Ethereum, BinanceTestnet } from '@thirdweb-dev/chains';
 
 export default function TabOneScreen() {
+  const sdk = useSDK();
+  const provider = sdk?.getProvider();
+  console.log(window.ethereum, 'asd');
+  console.log(
+    BinanceTestnet.networkId,
+    getProviderFromRpcUrl(
+      'https://bsc-testnet.blockpi.network/v1/rpc/public',
+      {}
+    )
+  );
+
+  const switchChain = useSwitchChain();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <TouchableOpacity onPress={() => switchChain(BinanceTestnet.chainId)}>
+        <Text>Switch Chain</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={async () => {
+          await provider?.request({
+            method: 'eth_requestAccounts',
+          });
+        }}
+      >
+        <Text>Switch Chain</Text>
+      </TouchableOpacity>
+
+      <ConnectWallet />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
+// Import polyfills
+import '@thirdweb-dev/react-native-compat';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { ThirdwebProvider } from '@thirdweb-dev/react-native';
+
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -49,11 +57,16 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <ThirdwebProvider
+      activeChain={'ethereum'}
+      clientId={'8d30cd5b80149184f1e17f092232930a'}
+    >
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        </Stack>
+      </ThemeProvider>
+    </ThirdwebProvider>
   );
 }
